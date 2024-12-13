3be05bbe4a6be309b3da90c045c66e3b911bc3e8
Alika <dangolalika2017@gmail.com>
2024-12-13 16:00:04 +0545

    added function

--- reducerFunction/src/Store.jsx
+++ reducerFunction/src/Store.jsx
@@ -1,51 +1,58 @@
+import { createStore } from "redux";
 
- import { createStore } from 'redux'
+// making a function to use easily and also makes dynamic
+// the folloeing function is used  for adding an ddeleting task
 
+const addTask = (data) => {
+  return { type: ADD_TASK, payload: data };
+};
 
-
+const deleteTask = (index) => {
+  return {
+    type: DELETE_TASK,
+    payload: index,
+  };
+};
 
 const ADD_TASK = "task/add";
-const DELETE_TASK = "task/delete"
-
+const DELETE_TASK = "task/delete";
 
 const initialState = {
-    
-        task: [],
-    
-}
+  task: [],
+};
 
-const reducerfunction =((state = initialState, action)=>{
-    switch (action.type) {
-        case ADD_TASK:
-            return{
-                ...state, task:[ ...state.task, action.payload]
-            }
-        case DELETE_TASK:
-            const updatedTask = state.task.filter((curtask, index)=>{
-                return index !== action.payload
+const reducerfunction = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      };
+    case DELETE_TASK:
+      const updatedTask = state.task.filter((curtask, index) => {
+        return index !== action.payload;
+      });
+      return {
+        ...state,
+        task: [updatedTask],
+      };
 
-            })
-            return{
-                ...state, task: [ updatedTask]
-            }
-
-    
-        default:
-            return state;
-    }
-
-})
-
-
+    default:
+      return state;
+  }
+};
 
 const store = createStore(reducerfunction);
-    console.log(store)
+console.log(store);
 
+console.log("initial", store.getState());
+// store.dispatch({ type: ADD_TASK, payload: "Hi alika" });
+store.dispatch(addTask("Hi Alika"));
+console.log("updated", store.getState());
 
-    console.log("initial", store.getState());
-    store.dispatch({type:ADD_TASK, payload:"Hi alika"});
-    console.log("updated", store.getState());
-    store.dispatch({type:ADD_TASK, payload: "hi roshana"});
-    console.log("updated", store.getState());
-    store.dispatch({type:DELETE_TASK, payload:1})
-    console.log("deleted", store.getState())
+store.dispatch({ type: ADD_TASK, payload: "hi roshana" });
+console.log("updated", store.getState());
+
+// store.dispatch({ type: DELETE_TASK, payload: 1 })
+store.dispatch(deleteTask(1));
+console.log("deleted", store.getState());
